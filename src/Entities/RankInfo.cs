6b9c7cb67ac00ad760e9a0198c890a75b09cead3
svr333 <vanrompaey.senne@protonmail.com>
2020-10-19 01:02:31 +0200

    Add support for locked accounts

--- src/Entities/RankInfo.cs
+++ src/Entities/RankInfo.cs
@@ -8,10 +8,11 @@ namespace GLR.Net.Entities
             get 
             {
                 if (Rank == Rank.Developer) return 480472;
-                if (Rank == Rank.Moderator) return 2605694;
-                if (Rank == Rank.Tester) return 16729674;
-                if (Rank == Rank.Donator) return 15710778;
-                if (Rank == Rank.Banned) return 16777215;
+                else if (Rank == Rank.Moderator) return 2605694;
+                else if (Rank == Rank.Tester) return 16729674;
+                else if (Rank == Rank.Donator) return 15710778;
+                else if (Rank == Rank.Locked) return 16777215;
+                else if (Rank == Rank.Banned) return 16777215;
                 else return 000000;
             }
         }
@@ -20,10 +21,11 @@ namespace GLR.Net.Entities
     public enum Rank
     {
         Banned = 0,
-        Member = 1,
-        Donator = 2,
-        Tester = 3,
-        Moderator = 4,
-        Developer = 5
+        Locked = 1,
+        Member = 2,
+        Donator = 3,
+        Tester = 4,
+        Moderator = 5,
+        Developer = 6
     }
 }
